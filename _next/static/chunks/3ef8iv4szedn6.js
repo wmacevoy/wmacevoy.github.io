872b72234a2f5b33465861a4d3efcd949a9d1e00
Warren MacEvoy <wmacevoy@gmail.com>
2026-08-19 11:17:19 -0600

    Updates

--- _next/static/chunks/3ef8iv4szedn6.js
+++ _next/static/chunks/3ef8iv4szedn6.js
@@ -3,6 +3,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/00cpl6gzwpv09.js",
   "static/chunks/0ta2fzd5aq9_g.js",
   "static/chunks/0srzr0_135d6e.js",
-  "static/chunks/40e64uvmkbuwb.css",
-  "static/chunks/turbopack-3_ql3x3-1n8hj.js"
+  "static/chunks/1wom62ycl5le0.css",
+  "static/chunks/turbopack-2t_qxt9uovb__.js"
 ])
